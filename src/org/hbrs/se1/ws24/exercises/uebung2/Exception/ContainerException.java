9ceb16a9a727a37bb0760aa4e364969421debc75
Florian <florian.paehler@smial.inf.h-brs.de>
2024-10-10 15:08:58 +0200

    comm

---
+++ src/org/hbrs/se1/ws24/exercises/uebung2/Exception/ContainerException.java
@@ -0,0 +1,14 @@
+package org.hbrs.se1.ws24.exercises.uebung2.Exception;
+
+import org.hbrs.se1.ws24.exercises.uebung2.Container.Container;
+import org.hbrs.se1.ws24.exercises.uebung2.Member.ConcreteMember;
+
+public class ContainerException extends Exception{
+
+    public ContainerException(String message){
+
+        super(message);
+
+    }
+
+}
